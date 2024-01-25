efec761d150252c7fabb2b138696cca0d7f6e985
Clement Julia <clement.julia13@gmail.com>
2024-01-25 13:13:38 +0100

    Update functional_tests.py

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,27 +1,26 @@
-import logging
 import os
-import shutil
+from typing import Union
 import unittest
 
 from pyBIG import Archive, LargeArchive
 
-logging.basicConfig(level=logging.INFO)
 
-TEST_FILE = "data\\ini\\weapon.ini"
+TEST_FILE = "read_me_for_test.txt"
 TEST_CONTENT = "john"
 TEST_ENCODING = "latin-1"
-TEST_ARCHIVE = "test_data/empty_archive.big"
+TEST_ARCHIVE = "tests/test_data/empty_archive.big"
 
 
 class BaseTestCases:
     class BaseTest(unittest.TestCase):
+        archive: Union[Archive, LargeArchive]
         def test_decode(self):
             contents = self.archive.read_file(TEST_FILE)
 
-            with open("test_data/weapon.ini", "rb") as f:
+            with open(f"tests/test_data/{TEST_FILE}", "rb") as f:
                 self.assertEqual(f.read(), contents)
 
-            self.assertEqual(len(self.archive.entries), 1236)
+            self.assertEqual(len(self.archive.entries), 1)
 
         def test_remove_file(self):
             self.archive.remove_file(TEST_FILE)
@@ -39,25 +38,29 @@ class BaseTestCases:
             self.assertEqual(contents, TEST_CONTENT)
 
         def test_add_file(self):
-            self.archive.add_file(f"{TEST_FILE}.inc", TEST_CONTENT.encode(TEST_ENCODING))
+            if self.archive.file_exists(TEST_FILE):
+                self.archive.remove_file(TEST_FILE)
+                self.archive.repack()
+
+            self.archive.add_file(TEST_FILE, TEST_CONTENT.encode(TEST_ENCODING))
             self.archive.repack()
 
-            contents = self.archive.read_file(f"{TEST_FILE}.inc").decode(TEST_ENCODING)
+            contents = self.archive.read_file(TEST_FILE).decode(TEST_ENCODING)
 
             self.assertEqual(contents, TEST_CONTENT)
 
         def test_extract_and_load(self):
-            self.archive.extract("test_data/output")
-            self.assertTrue(os.path.exists("test_data/output/data/ini/weapon.ini"))
+            self.archive.extract("tests/test_data/output")
+            self.assertTrue(os.path.exists(f"tests/test_data/output/{TEST_FILE}"))
 
-            new_archive = Archive.from_directory("test_data/output")
+            new_archive = Archive.from_directory("tests/test_data/output")
             self.assertIn(TEST_FILE, new_archive.entries)
 
-            new_archive.save("test_data/output/test.big")
-            self.assertTrue(os.path.exists("test_data/output/test.big"))
+            new_archive.save("tests/test_data/output/test.big")
+            self.assertTrue(os.path.exists("tests/test_data/output/test.big"))
 
-            shutil.rmtree("test_data/output/data")
-            os.remove("test_data/output/test.big")
+            os.remove(f"tests/test_data/output/{TEST_FILE}")
+            os.remove("tests/test_data/output/test.big")
 
         def test_utils(self):
             self.archive.file_list()
@@ -65,36 +68,36 @@ class BaseTestCases:
 
 class TestArchive(BaseTestCases.BaseTest):
     def setUp(self):
-        with open("test_data/__edain_data.big", "rb") as f:
+        with open("tests/test_data/test_big.big", "rb") as f:
             self.archive = Archive(f.read())
 
     def test_empty_archive(self):
         archive = Archive.empty()
-        archive.add_file(f"{TEST_FILE}.inc", TEST_CONTENT.encode(TEST_ENCODING))
+        archive.add_file(TEST_FILE, TEST_CONTENT.encode(TEST_ENCODING))
         archive.repack()
 
-        self.assertIn(f"{TEST_FILE}.inc", archive.entries)
+        self.assertIn(TEST_FILE, archive.entries)
 
 
 class TestLargeArchive(BaseTestCases.BaseTest):
     def setUp(self):
-        self.archive = LargeArchive("test_data/__edain_data.big")
+        self.archive = LargeArchive("tests/test_data/test_big.big")
 
-        if self.archive.file_exists(f"{TEST_FILE}.inc"):
-            self.archive.remove_file(f"{TEST_FILE}.inc")
+        if self.archive.file_exists(TEST_FILE):
+            self.archive.remove_file(TEST_FILE)
 
         if not self.archive.file_exists(TEST_FILE):
-            with open("test_data/weapon.ini", "rb") as f:
+            with open(f"tests/test_data/{TEST_FILE}", "rb") as f:
                 self.archive.add_file(TEST_FILE, f.read())
 
         self.archive.repack()
 
     def test_empty_archive(self):
         archive = LargeArchive.empty(TEST_ARCHIVE)
-        archive.add_file(f"{TEST_FILE}.inc", TEST_CONTENT.encode(TEST_ENCODING))
+        archive.add_file(TEST_FILE, TEST_CONTENT.encode(TEST_ENCODING))
         archive.repack()
 
-        self.assertIn(f"{TEST_FILE}.inc", archive.entries)
+        self.assertIn(TEST_FILE, archive.entries)
         os.remove(TEST_ARCHIVE)
 
 
